2e9c828a09519a3223d2a61688585c27ff366f87
iamalexhu <62089371+imalexhu@users.noreply.github.com>
2021-11-14 20:59:54 +1030

    heap sort

--- code/4/heap.cpp
+++ code/4/heap.cpp
@@ -5,60 +5,93 @@ class Heap
 {
 public:
     vector<int> heap;
+
     int getLeft(int i)
     {
-        return 2 * i + 1;
+        int index = 2 * i + 1;
+        if (index < heap.size())
+            return index;
+        else
+            return -1;
     }
     int getRight(int i)
     {
-        return 2 * i + 2;
+        int index = 2 * i + 2;
+        if (index < heap.size())
+            return index;
+        else
+            return -1;
     }
     int getParent(int i)
     {
-        return (i - 1) / 2;
+        int index = (i - 1) / 2;
+        if (index >= 0)
+            return index;
+        else
+            return -1;
     }
 
-    void bubbleDown(int i)
+    void bubbleDown()
     {
-        int index = i;
-        while (getLeft(index) < heap.size() && heap[index] > heap[getLeft(index)])
+        int index = 0;
+        int smallestIndex = getLeft(index);
+        while (smallestIndex != -1 && heap[index] > heap[smallestIndex])
         {
-            int minI = getLeft(index);
-            int min = heap[getLeft(index)];
-            if (getRight(index) < heap.size() && heap[getRight(index)] < min)
+            // cout<< " left index " << smallestIndex <<endl;
+
+            int rightIndex = getRight(index);
+            // cout<< " Right index " << rightIndex <<endl;
+            if (rightIndex != -1 && heap[rightIndex] < heap[smallestIndex])
             {
-                min = heap[getRight(index)];
-                minI = getRight(index);
+                // cout << "Why are you running " << endl;
+                smallestIndex = rightIndex;
             }
-            swap(heap[minI], heap[index]);
+            // cout << "SmallestIndex : " << smallestIndex << "  "
+            //      << "index : " << index << endl;
+            // cout << " smallestIndex val : " << heap[smallestIndex] << " : "
+            //      << "Index Val : " << heap[index] << endl;
+            swap(heap[smallestIndex], heap[index]);
+            index = smallestIndex;
+            smallestIndex = getLeft(index);
         }
     }
 
     int pop()
     {
-        int index = 0;
-        int val = heap[index];
+        if(heap.size()==1){
+            int val = heap.back();
+            heap.pop_back();
+            return val;
+        }
+        int val = heap[0];
         heap.erase(heap.begin());
         int temp = heap.back();
         heap.pop_back();
         heap.insert(heap.begin(), temp);
-        bubbleDown(index);
+        bubbleDown();
         return val;
     }
 
-    void bubbleUp(int i)
+    void bubbleUp()
     {
-        int index = i;
-        while (getParent(i) > 0 && heap[getParent(i)] > heap[index])
+        int index = heap.size() - 1;
+        int parentIndex = getParent(index);
+        while (heap[index] < heap[parentIndex])
         {
-            swap(heap[index], heap[getParent(i)]);
+            swap(heap[index], heap[parentIndex]);
+            index = parentIndex;
+            parentIndex = getParent(index);
+            // cout << "parentIndex : " << parentIndex << "  "
+            //      << "index : " << index << endl;
+            // cout << " Parent val : " << heap[parentIndex] << " : "
+            //      << "Index Val : " << heap[index] << endl;
         }
     }
 
     void add(int val)
     {
         heap.push_back(val);
-        bubbleUp(heap.size() - 1);
+        bubbleUp();
     }
 
     void print()
@@ -68,24 +101,48 @@ public:
             cout << i << endl;
         }
     }
+
+    void heapSort(vector<int> &nums)
+    {
+        for (auto i : nums)
+        {
+            add(i);
+        }
+        nums.clear();
+        print();
+        cout << "new " << endl;
+        while (!heap.empty())
+        {
+            int temp = pop();
+            nums.push_back(temp);
+        }
+    }
 };
 
 int main()
 {
 
-    vector<int> nums = {5, 4, 3, 2, 1};
+    vector<int> nums = {5, 4, 3, 2};
 
     Heap *h = new Heap();
-    h->add(1);
-    h->add(3);
-    h->add(66);
-    h->add(17);
-    h->add(2);
-    h->print();
-    h->pop();
-    h->print();
-    h->pop();
-    h->print();
+    // h->add(1);
+    // h->add(2);
+    // h->add(3);
+    // h->add(4);
+    // h->add(5);
+    // h->print();
+    // h->pop();
+    // h->print();
+    // h->add(1);
+    // h->pop();
+    // h->print();
+
+    h->heapSort(nums);
+
+    for (auto i : nums)
+    {
+        cout << i << endl;
+    }
 
     return 0;
 }
